8eb1ebe7d2f688ec6b301ec226cb9bde3a4cd2e4
crystalmilliken <crystal@millikenconsulting.com>
2018-01-12 11:19:02 -0500

    messing around

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -45,17 +45,31 @@ class CreateGame{
         this.time--;
         this.isrunning = true;
         document.getElementById("timer").innerText = `Time Remaing: ${this.time}`;
+        document.getElementById("gif").innerHTML = "<img src='assets/images/walking.png' alt='walking turtle'>";
+        document.getElementById("personStatus").innerHTML = "<img src='assets/images/happyPerson.png' alt='happy person'>";
         if(this.time <= 0){
+            if(this.tempArray.length <= 0){
+                alert("game over");
+                this.wrong = 0;
+                this.right = 0;
+                document.getElementById("question").innerText = "";
+                document.getElementById("answers").innerHTML = "";
+                this.generateQuestions();
+            }else{
             this.wrong++;
             this.isrunning = false;
             clearInterval(this.setInterval);
             this.generateQuestions();
+            document.getElementById("gif").innerHTML = "<img src='assets/images/turtleFire.png' alt='walking turtle'>";
+            document.getElementById("personStatus").innerHTML = "<img src='assets/images/sadPerson.png' alt='sad person'>";
+            }
         }
         if(this.time > 15){
             document.getElementById("timer").classList.remove("danger");
             
         }else if(this.time <= 15 && this.time > 0){
-            document.getElementById("timer").classList = "danger";
+            document.getElementById("timer").classList.toggle("danger");
+           
         }
     }
     generateAnswers(fakeAndReal){
@@ -79,6 +93,8 @@ class CreateGame{
     }
 
     generateQuestions(){
+        document.getElementById("gif").innerHTML = "<img src='assets/images/walking.png' alt='walking turtle'>";
+        document.getElementById("personStatus").innerHTML = "<img src='assets/images/happyPerson.png' alt='happy person'>";
         document.getElementById("right").innerHTML = `Right: ${this.right}`;
         document.getElementById("wrong").innerHTML = `Wrong: ${this.wrong}`;
         document.getElementById("result").innerHTML ="";
@@ -108,7 +124,10 @@ class CreateGame{
                 alert("game over");
                 document.getElementById("question").innerText = "";
                 document.getElementById("answers").innerHTML = "";
+                this.wrong = 0;
+                this.right = 0;
                 this.generateQuestions();
+                
             }else{
                 this.isrunning = true;
                 this.time = 30;
@@ -129,7 +148,8 @@ class CreateGame{
             document.getElementById("answers").innerHTML = "";
             document.getElementById("result").innerHTML = `${this.currentQuestion.answer}<img src='assets/images/${this.happyIm[ranNum]}'>`;
             clearInterval(this.setInterval);
-            setTimeout(this.generateQuestions.bind(this),5000);
+             document.getElementById("gif").innerHTML = "<img src='assets/images/turtleDissapointed.png' alt='dissapointed turtle'>";
+            setTimeout(this.generateQuestions.bind(this),2000);
             
         }else{
             this.wrong++;
@@ -139,7 +159,9 @@ class CreateGame{
             document.getElementById("answers").innerHTML = "";
             document.getElementById("result").innerHTML = `${this.currentQuestion.answer}<br><img src='assets/images/${this.bummerIm[ranNum]}'>`;
             clearInterval(this.setInterval);
-             setTimeout(this.generateQuestions.bind(this),5000);
+             document.getElementById("gif").innerHTML = "<img src='assets/images/turtleFire.png' alt='walking turtle'>";
+            document.getElementById("personStatus").innerHTML = "<img src='assets/images/sadPerson.png' alt='sad person'>";
+             setTimeout(this.generateQuestions.bind(this),2000);
         }
     }
     toggleStart(){
@@ -153,6 +175,16 @@ class CreateGame{
         }
     }
 }
-
+function getData(){
+let xmlRequestObject = new XMLHttpRequest();
+xmlRequestObject.onreadystatechange= function(){
+    if(this.readyState === 4 && this.status === 200){
+        console.log(this.responseText);
+    }
+}
+    xmlRequestObject.open("Get","assets/javascript/data.js");
+    xmlRequestObject.send();
+}
+getData();
 let triviaGame = new CreateGame(questions, fakeAnswers, happyIm, bummerIm);
 document.getElementById("start").addEventListener("click", function(){triviaGame.toggleStart()}.bind(this));
